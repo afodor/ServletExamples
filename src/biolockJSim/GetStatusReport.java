6ef5314babb72e9d4ef7a8d42ff0d3b7ff0f991f
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-07-02 13:18:25 -0400

    about to test report servlet

---
+++ src/biolockJSim/GetStatusReport.java
@@ -0,0 +1,45 @@
+package biolockJSim;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetStatusReport extends HttpServlet
+{
+	public static final String JOB_ID = "JOB_ID";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		int id = Integer.parseInt(req.getParameter(JOB_ID));
+		
+		BiolockJSim bSim = BiolockJSim.runMap.get(id);
+		
+		PrintWriter out = resp.getWriter();
+		
+		for(int x=0; x < bSim.getNumNodes(); x++)
+		{
+			BiolockJSimNode bNode = bSim.getNode(x);
+			
+			out.write("{\n");
+			
+			out.write("nodeName : " + bNode.getName() + ", \n");
+
+			out.write("job Name : " + bNode.getName() + ", \n");
+			
+			out.write("progress : " + bNode.getProgress() + " \n");
+			
+			out.write("}\n");
+			
+			if( x +1 < bSim.getNumNodes())
+				out.write(",\n");
+		}
+		
+		
+		out.flush();  out.close();
+	}
+}
