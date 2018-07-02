6c7f59fa482124645e48bc84a6024807fd639bba
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-07-02 10:23:15 -0400

    initial commit

---
+++ src/biolockJSim/KickOffBiolockJSim.java
@@ -0,0 +1,27 @@
+package biolockJSim;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class KickOffBiolockJSim extends HttpServlet
+{
+
+	AtomicInteger idCounter = new AtomicInteger(0);
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		out.write("{jobID : " + idCounter.getAndIncrement() + "}\n");
+		
+		out.flush();  out.close();
+	}
+}
